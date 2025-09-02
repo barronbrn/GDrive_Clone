985c9d0a82048071b0dedfcaec90fc68bc528b87
barronbrn <qwebaron7@gmail.com>
2025-09-02 08:04:48 +0700

    Fitur CRUD

--- app/Models/File.php
+++ app/Models/File.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class File extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'path',
+        'mime_type',
+        'size',
+        'is_folder',
+        'created_by',
+        'parent_id',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,30 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index'])
-    ->name('dashboard');
+// Halaman utama (root)
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+// Rute untuk menampilkan isi folder
+Route::get('/folders/{folder}', [DashboardController::class, 'index'])->name('dashboard.folder');
 
-// Rute untuk mengelola profil pengguna, juga dilindungi oleh login.
-Route::middleware('auth')->group(function () {
+// Grup rute yang memerlukan login
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Rute Profil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    // Rute Menu Navigasi
     Route::get('/recent', [DashboardController::class, 'recent'])->name('recent');
     Route::get('/trash', [DashboardController::class, 'trash'])->name('trash');
+
+    // Rute Aksi CRUD
+    Route::post('/folder/create', [DashboardController::class, 'createFolder'])->name('folder.create');
+    Route::post('/file/upload', [DashboardController::class, 'uploadFile'])->name('file.upload');
+    Route::delete('/file/delete/{file}', [DashboardController::class, 'delete'])->name('file.delete');
+
+    // Rute Aksi File
+    Route::get('/file/download/{file}', [DashboardController::class, 'download'])->name('file.download');
+    Route::get('/file/preview/{file}', [DashboardController::class, 'preview'])->name('file.preview');
 });
 
-// Memuat rute-rute autentikasi (login, logout, register, dll.)
 require __DIR__ . '/auth.php';
