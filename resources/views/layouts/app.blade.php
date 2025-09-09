d2e618b9f4799e65f152592c463f619fea8ed439
fathan M <Fathanra123@gmail.com>
2025-09-09 11:28:53 +0700

    Fitur Slide Navigasi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
              x-transition:enter="transition ease-in-out duration-300"
              x-transition:enter-start="opacity-0 transform -translate-x-full"
              x-transition:enter-end="opacity-100 transform translate-x-0"
-             x-transition:leave="transition ease-in-out duration-300"
+             x-transition:leave="transition ease-in duration-200"
              x-transition:leave-start="opacity-100 transform translate-x-0"
              x-transition:leave-end="opacity-0 transform -translate-x-full">
             @include('partials.sidebar')
