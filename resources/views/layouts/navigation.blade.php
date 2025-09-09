aebe09240f4b9dbeea209e11c373473e6753c11c
unknown <aksalpriadi@gmail.com>
2025-09-09 15:02:29 +0700

    ProfileLogin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,19 +22,19 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
+                        <button class="flex items-center justify-center h-8 w-8 rounded-full bg-gray-200 text-gray-600 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition ease-in-out duration-150">
+                            <div class="font-medium text-sm">{{ strtoupper(substr(Auth::user()->name, 0, 1)) }}</div>
                         </button>
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
+                        <div class="px-4 py-3 border-b border-gray-200">
+                            <p class="text-sm font-semibold text-gray-800">{{ Auth::user()->name }}</p>
+                            <p class="text-xs text-gray-500">{{ Auth::user()->email }}</p>
+                        </div>
+
+                        <x-dropdown-link :href="route('profile.edit')" class="flex items-center">
+                            <span class="material-symbols-outlined text-base mr-2">account_circle</span>
                             {{ __('Profile') }}
                         </x-dropdown-link>
 
@@ -44,7 +44,9 @@
 
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                                                this.closest('form').submit();"
+                                    class="flex items-center">
+                                <span class="material-symbols-outlined text-base mr-2">logout</span>
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -38,26 +38,39 @@
         <!-- Right Section: Auth/Guest Dropdown -->
         @auth
             <!-- Dropdown Profile -->
-            <div x-data="{ open: false }" class="relative ml-4">
+            <div x-data="{ open: false, tooltip: false }" class="relative ml-4">
                 <button 
-                    @click="open = !open" 
-                    class="flex items-center space-x-2 text-sm font-medium focus:outline-none transition duration-150 ease-in-out rounded-full p-1 hover:bg-white/10">
-                    <img class="h-10 w-10 rounded-full object-cover border-2 border-white/50" src="https://ui-avatars.com/api/?name={{ urlencode(Auth::user()->name) }}&color=FFFFFF&background=00529B" alt="{{ Auth::user()->name }}">
-                    <div class="hidden sm:block text-sm font-medium">{{ Auth::user()->name }}</div>
-                    <div class="ml-1 hidden sm:block">
-                        <svg class="fill-current h-4 w-4 text-white/80" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
+                    @click="open = !open"
+                    @mouseenter="tooltip = true"
+                    @mouseleave="tooltip = false"
+                    class="flex items-center justify-center h-10 w-10 rounded-full bg-white/20 text-white focus:outline-none transition duration-150 ease-in-out hover:bg-white/30 border-2 border-white/50">
+                    <div class="font-bold text-xl">{{ strtoupper(substr(Auth::user()->name, 0, 1)) }}</div>
                 </button>
+                <div 
+                    x-show="tooltip && !open"
+                    x-transition
+                    class="absolute left-1/2 -translate-x-1/2 top-full mt-2 w-max bg-gray-800 text-white text-sm rounded-md px-3 py-1 pointer-events-none z-50">
+                    {{ Auth::user()->name }}
+                    <div class="text-xs text-gray-400">{{ Auth::user()->email }}</div>
+                </div>
 
-                <div x-show="open" @click.outside="open = false" x-cloak class="absolute z-50 mt-2 w-48 rounded-xl shadow-lg origin-top-right right-0 bg-white overflow-hidden">
+                <div x-show="open" @click.outside="open = false" x-cloak class="absolute z-50 mt-2 w-56 rounded-xl shadow-lg origin-top-right right-0 bg-white overflow-hidden ring-1 ring-black ring-opacity-5">
+                    <div class="px-4 py-3 border-b border-gray-200">
+                        <p class="text-sm font-bold text-gray-800">{{ Auth::user()->name }}</p>
+                        <p class="text-xs text-gray-500">{{ Auth::user()->email }}</p>
+                    </div>
                     <div class="py-1">
-                        <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profile</a>
+                        <a href="{{ route('profile.edit') }}" class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                            <span class="material-symbols-outlined text-base mr-2">account_circle</span>
+                            Profile
+                        </a>
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" 
-                               class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Log Out</a>
+                               class="flex items-center w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-red-50">
+                                <span class="material-symbols-outlined text-base mr-2">logout</span>
+                                Log Out
+                            </a>
                         </form>
                     </div>
                 </div>
